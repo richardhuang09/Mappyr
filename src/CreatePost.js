6d89836d9c1aa4b359f1b3634ba4e266aa550959
Richard Huang <rhuang09@gmail.com>
2016-10-20 15:39:18 -0400

    Text input field added to CreatePost

--- src/CreatePost.js
+++ src/CreatePost.js
@@ -1,5 +1,10 @@
 import React, { Component, PropTypes } from 'react';
-import { View, Text, TouchableHighlight } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableHighlight,
+  TextInput,
+} from 'react-native';
 
 export default class CreatePost extends Component {
   static propTypes = {
@@ -7,13 +12,23 @@ export default class CreatePost extends Component {
     onForward: PropTypes.func.isRequired,
     onBack: PropTypes.func.isRequired,
   }
+
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
   render() {
     return (
-      <View>
-        <Text>Current Scene: { this.props.title }</Text>
-        <TouchableHighlight onPress={this.props.onForward}>
-          <Text>Tap me to load the next scene</Text>
-        </TouchableHighlight>
+      <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Leave your wisdom here"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text}
+        </Text>
         <TouchableHighlight onPress={this.props.onBack}>
           <Text>Tap me to go back</Text>
         </TouchableHighlight>
